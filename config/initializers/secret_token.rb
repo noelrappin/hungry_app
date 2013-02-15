15e8ae173bf8ba9dae22e5ea01d32005782e3590
Noel Rappin <noel@groupon.com>
2013-02-15 10:58:40 -0600

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HungryApp::Application.config.secret_token = 'f7cb5fd24ce98e9dd1708d3c70ffc4cf6f8ecc934572577153a47697422fe6e2c1f137905c123f109ce320619fb96e70a6dd2de1b24db6b38e3ccc019f9530d4'
