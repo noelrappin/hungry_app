53ebed03bb436f420fe1f4589a3c219aacd113e3
Noel Rappin <noel@groupon.com>
2013-02-15 14:17:01 -0600

    more migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130215193406) do
+ActiveRecord::Schema.define(:version => 20130215201557) do
 
   create_table "coupons", :force => true do |t|
     t.integer  "restaurant_id"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(:version => 20130215193406) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "category"
+    t.float    "lat"
+    t.float    "lon"
   end
 
   create_table "users", :force => true do |t|
@@ -51,6 +53,8 @@ ActiveRecord::Schema.define(:version => 20130215193406) do
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
     t.text     "favorite_categories"
+    t.float    "lat"
+    t.float    "lon"
   end
 
 end
